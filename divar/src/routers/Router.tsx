7aca6a6068123a9a4a1feb0603fb27526e99ab13
mohammadpy8 <mohammadpy8@gmail.com>
2024-02-02 10:38:10 +0330

    router-access

--- divar/src/routers/Router.tsx
+++ divar/src/routers/Router.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
 import DashboardPage from '../pages/DashboardPage'
 import AuthPage from '../pages/AuthPage'
@@ -14,9 +14,24 @@ const Router = () => {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/auth" element={<AuthPage />} />
-      <Route path="/admin" element={<AdminPage />} />
+      <Route
+        path="/dashboard"
+        element={data ? <DashboardPage /> : <Navigate to="/auth" />}
+      />
+      <Route
+        path="/auth"
+        element={data ? <Navigate to="/dashboard" /> : <AuthPage />}
+      />
+      <Route
+        path="/admin"
+        element={
+          data && data.data.role === 'ADMIN' ? (
+            <AdminPage />
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
